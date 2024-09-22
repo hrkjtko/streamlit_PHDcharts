133b1ee37c807671918716d823e527401eb1960b
hrkjt <168939068+hrkjt@users.noreply.github.com>
2024-09-22 18:43:29 +0900

    Update app.py
    result

--- app.py
+++ app.py
@@ -692,7 +692,8 @@ if submit_button:
       st.write('経過観察した場合のグラフを表示します')
       for parameter in parameters:
         line_plot(parameter, filtered_df_co)
-        make_table(parameter, filtered_df_co, co = True)
+        result = make_table(parameter, filtered_df_co, co = True)
+        st.dataframe(result, width=800)
 
     #df_vis = takamatsu(filtered_df_tx_pre_post)
     #st.dataframe(df_vis)
