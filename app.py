8ef32cc462c8e5513a7de0a44257fdbed864f49a
hrkjt <168939068+hrkjt@users.noreply.github.com>
2024-09-29 17:14:18 +0900

    Update app.py
    df = df.sort_values('月齢')

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ response = requests.get(url)
 data = response.json()
 
 df = pd.DataFrame(data['経過'])
+df = df.sort_values('月齢')
 
 parameters = ['月齢', '前後径', '左右径', '頭囲', '短頭率', '前頭部対称率', 'CA', '後頭部対称率', 'CVAI', 'CI']
 df[parameters] = df[parameters].apply(pd.to_numeric, errors='coerce')
