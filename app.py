e8a472b7bfaa936e0a7a1aecd3d765ad3112efa9
hrkjtko <jmrbx767@keio.jp>
2025-02-10 10:43:55 +0900

    Update app.py
    治療による

--- app.py
+++ app.py
@@ -860,7 +860,7 @@ def line_plot(parameter, df, df1=None):
 
   fig.update_xaxes(range = [df['月齢'].min()-2,df['月齢'].max()+2])
   fig.update_yaxes(range = [df[parameter].min()-2,df[parameter].max()+2])
-  fig.update_layout(width=900, title='経過観察前後の' + parameter + 'の変化')
+  fig.update_layout(width=900, title='治療による' + parameter + 'の変化')
 
   st.plotly_chart(fig)
 
